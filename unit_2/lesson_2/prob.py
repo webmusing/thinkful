0cda1e41163c681061138d25d65d8e182202afa6
Jonathan Moo <webmusing@gmail.com>
2015-11-24 20:35:54 +0800

    Added prob.py for unit 2, lesson 2 section 1

--- unit_2/lesson_2/prob.py
+++ unit_2/lesson_2/prob.py
@@ -2,6 +2,16 @@ import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 
+# Box Plot
+x = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
+plt.boxplot(x)
+plt.show()
+
+# Histogram Plot
+plt.hist(x, histtype='bar')
+plt.show()
+
+# QQ Plots
 plt.figure()
 test_data = np.random.normal(size=1000)   
 graph1 = stats.probplot(test_data, dist="norm", plot=plt)
