7341dc08dc07b8732c3886182864a94021ec371e
Jonathan Moo <webmusing@gmail.com>
2015-11-24 21:41:10 +0800

    Unit 2 2 lesson 2 section 2

---
+++ unit_2/lesson_2/prob_lending_club.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+
+loansData = pd.read_csv('loansData.csv')
+
+loansData.dropna(inplace=True)
+
+loansData.boxplot(column='Amount.Funded.By.Investors')
+
+plt.show()
+
+# Histogram Plot
+loansData.hist(column='Amount.Funded.By.Investors')
+plt.show()
+
+plt.figure()
+
+graph = stats.probplot(loansData['Amount.Funded.By.Investors'], dist="norm", plot=plt)
+
+plt.show()
+
+loansData.boxplot(column='Amount.Requested')
+
+plt.show()
+
+# Histogram Plot
+loansData.hist(column='Amount.Requested')
+plt.show()
+
+plt.figure()
+
+graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
+
+plt.show()
