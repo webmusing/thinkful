f11dc867a9d5da25c46f7f2fb554fd43e2a7d540
Jonathan Moo <webmusing@gmail.com>
2015-12-29 13:55:21 +0800

    Added chi_test.py

---
+++ unit_2/lesson_2/chi_test.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from scipy import stats
+import collections
+
+# Load the reduced version of the Lending Club Dataset
+loansData = pd.read_csv('loansData.csv')
+# Drop null rows
+loansData.dropna(inplace=True)
+
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+
+plt.figure()
+plt.bar(freq.keys(), freq.values(), width=1)
+plt.show()
+
+chi, p = stats.chisquare(freq.values())
+
+print chi, p
